2aa62061c142508a57953128691da1c9968ba4b8
J8820LONG <891893318@qq.com>
2022-11-18 22:51:38 +0800

    学习记录

--- day29（IOStream）/Copy/src/CopyFile.java
+++ day29（IOStream）/Copy/src/CopyFile.java
@@ -1,25 +1,29 @@
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class CopyFile {
     public void copyFile(String copyedPath, String copyPath){
         FileInputStream streamin = null;
         FileOutputStream streamout = null;
+        BufferedInputStream bufin = null;
+        BufferedOutputStream bufout = null;
         try {
             //1.实例化File
             File file = new File(copyedPath);
             File filecopy = new File(copyPath);
             //2.实例化流
+                //2.1 实例化输入输出流
             streamin = new FileInputStream(file);
             streamout = new  FileOutputStream(filecopy);
+                //2.2 实例化缓冲流
+            bufin = new BufferedInputStream(streamin);
+            bufout = new BufferedOutputStream(streamout);
             //3.文件的输入输出
             byte[] message = new byte[5];
             int len;
-            while ((len = streamin.read(message)) != -1){
+            //使用 bufin.read(message)，不然缓冲流则没意义
+            while ((len = bufin.read(message)) != -1){
                 String str = new String(message,0,len);
                 System.out.println(str);
                 streamout.write(message,0,len);
@@ -29,6 +33,18 @@ public class CopyFile {
         }
         finally {
             //4.流的关闭
+            try {
+                if (bufin != null)
+                    bufin.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (bufout != null)
+                    bufin.close();
+            } catch (IOException e) {
+               e.printStackTrace();
+            }
             try {
                 if (streamin != null)
                     streamin.close();
